ce7b16b770c2e17be927303a962deb7080a86213
daniel <d.harman@shu.ac.uk>
2020-05-04 19:24:34 +0100

    FIX THE TESTS

--- src/test/kotlin/uk/me/danielharman/kotlinspringbot/KotlinSpringBotApplicationTests.kt
+++ src/test/kotlin/uk/me/danielharman/kotlinspringbot/KotlinSpringBotApplicationTests.kt
@@ -1,4 +1,4 @@
-package uk.me.danielharman.KotlinSpringBot
+package uk.me.danielharman.kotlinspringbot
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
