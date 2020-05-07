78872a63882475896a8318b870d7f2d9e5e9b0ed
daniel <d.harman@shu.ac.uk>
2020-05-07 09:16:50 +0100

    Don't run SetupService.kt when testing

--- src/main/kotlin/uk/me/danielharman/kotlinspringbot/SetupService.kt
+++ src/main/kotlin/uk/me/danielharman/kotlinspringbot/SetupService.kt
@@ -1,12 +1,14 @@
 package uk.me.danielharman.kotlinspringbot
 
 import akka.actor.ActorRef
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import uk.me.danielharman.kotlinspringbot.ApplicationLogger.logger
 import uk.me.danielharman.kotlinspringbot.actors.ActorProvider
 import javax.annotation.PostConstruct
 
 @Component
+@Profile("!test")
 class SetupService(var actorProvider: ActorProvider) {
 
     @PostConstruct
--- src/test/kotlin/uk/me/danielharman/kotlinspringbot/KotlinSpringBotApplicationTests.kt
+++ src/test/kotlin/uk/me/danielharman/kotlinspringbot/KotlinSpringBotApplicationTests.kt
@@ -2,8 +2,10 @@ package uk.me.danielharman.kotlinspringbot
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KotlinSpringBotApplicationTests {
 
 	@Test
