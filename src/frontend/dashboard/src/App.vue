61674c35dc575eeda1346d2a3785ff0f5d2c5188
Daniel Harman <danielgharman@outlook.com>
2020-05-15 00:03:45 +0100

    It's Vue time

---
+++ src/frontend/dashboard/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+      <a class="navbar-brand" href="#">KotBot</a>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/frontend/dashboard/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container w-50">
+        <div class="card">
+        <div class="card-header">
+            Status
+        </div>
+        <div class="card-body">
+            <button class="btn btn-info" @click="onRestartDiscord">Restart Discord</button>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        methods: {
+            onRestartDiscord() {
+                console.log("Click!")
+            }
+        }
+    }
+</script>
