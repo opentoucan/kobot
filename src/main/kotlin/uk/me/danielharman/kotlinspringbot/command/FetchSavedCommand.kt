b3be3a23ee3ff64f5efe73e93062211c6cb4ff89
daniel <d.harman@shu.ac.uk>
2020-06-03 00:16:17 +0100

    Made saved commands list nicer

--- src/main/kotlin/uk/me/danielharman/kotlinspringbot/command/FetchSavedCommand.kt
+++ src/main/kotlin/uk/me/danielharman/kotlinspringbot/command/FetchSavedCommand.kt
@@ -5,20 +5,27 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import uk.me.danielharman.kotlinspringbot.listeners.helpers.Embeds
 import uk.me.danielharman.kotlinspringbot.services.GuildService
 
-class FetchSavedCommand(private val guildService: GuildService): Command {
+class FetchSavedCommand(private val guildService: GuildService) : Command {
     override fun execute(event: GuildMessageReceivedEvent) {
         val guild = guildService.getGuild(event.guild.id)
-        if(guild == null) {
+        if (guild == null) {
             event.channel.sendMessage(Embeds.createErrorEmbed("Guild not found")).queue()
             return
         }
-        val stringBuilder = StringBuilder()
-        guild.customCommands.entries.forEach { (s, _) -> stringBuilder.append("$s ") }
-
-        event.channel.sendMessage(EmbedBuilder()
+        val builder = EmbedBuilder()
                 .setTitle("Saved commands")
                 .setColor(0x9d03fc)
-                .setDescription(stringBuilder.toString())
-                .build()).queue()
+
+
+        val stringBuilder = StringBuilder()
+
+        guild.customCommands.entries.sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER, { it.key }))
+                .forEach { (k, v) -> builder.addField(k, truncate(v.value, 20), true) }
+
+        event.channel.sendMessage(builder.build()).queue()
     }
+
+    private fun truncate(str: String, limit: Int): String =
+            if (str.length <= limit) str else str.slice(IntRange(0, limit))
+
 }
