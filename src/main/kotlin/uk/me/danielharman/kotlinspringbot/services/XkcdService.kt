e969aa655d6eb6188b12b4eb22567d8a25a5b2a0
daniel <d.harman@shu.ac.uk>
2020-07-29 13:44:46 +0100

    Create database entry for keeping track of latest XKCD

--- src/main/kotlin/uk/me/danielharman/kotlinspringbot/services/XkcdService.kt
+++ src/main/kotlin/uk/me/danielharman/kotlinspringbot/services/XkcdService.kt
@@ -7,14 +7,22 @@ import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.readText
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Service
+import uk.me.danielharman.kotlinspringbot.SchemaUpdater
 import uk.me.danielharman.kotlinspringbot.models.XkcdComic
 
 @Service
-class XkcdService {
+class XkcdService(val mongoOperations: MongoOperations) {
 
     private val latestUrl = "https://xkcd.com/info.0.json"
     private val comicUrl = "https://xkcd.com/%d/info.0.json"
+
+    data class XkcdLatest(val num: Int)
 
     fun getLatestComic(): XkcdComic {
 
@@ -33,6 +41,18 @@ class XkcdService {
 
         val s = runBlocking { response.readText() }
         return Json.parse(XkcdComic.serializer(), s )
+    }
+
+    private fun setLast(ver: Int): Int {
+        mongoOperations.upsert(Query.query(Criteria.where("_id").`is`("latest")),
+                Update.update("schemaVersion", ver), SchemaUpdater.ApplicationOpts::class.java, "XkcdLatest")
+
+        return getLast()?.num ?: 0
+    }
+
+    private fun getLast(): XkcdLatest? {
+        return mongoOperations.findOne(Query.query(Criteria.where("_id").`is`("latest")),
+                XkcdLatest::class.java, "XkcdLatest")
     }
 
 }
