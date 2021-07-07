535888b6e09590e1325fd9bf9fd551ca0cbd2c1f
Daniel <danielgharman@outlook.com>
2021-07-07 22:16:55 +0100

    Update dependencies to latest versions Update Gradle version Target Java 16

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,19 @@ import me.qoomon.gradle.gitversioning.GitVersioningPluginConfig
 import me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.*
 
 plugins {
-    id("org.springframework.boot") version "2.4.4"
+    id("org.springframework.boot") version "2.5.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.0"
-    kotlin("plugin.spring") version "1.5.0"
-    kotlin("kapt") version "1.5.0"
-    kotlin("plugin.serialization") version "1.5.0"
-    id("me.qoomon.git-versioning") version "4.2.0"
-    id("org.barfuin.gradle.jacocolog") version "1.2.4"
+    kotlin("jvm") version "1.5.20"
+    kotlin("plugin.spring") version "1.5.20"
+    kotlin("kapt") version "1.5.20"
+    kotlin("plugin.serialization") version "1.5.20"
+    id("me.qoomon.git-versioning") version "4.2.1"
+    id("org.barfuin.gradle.jacocolog") version "2.0.0"
     jacoco
 }
 
 group = "uk.me.danielharman"
-java.sourceCompatibility = JavaVersion.VERSION_15
+java.sourceCompatibility = JavaVersion.VERSION_16
 
 version = "Kobot"
 gitVersioning.apply(closureOf<GitVersioningPluginConfig> {
@@ -56,11 +56,11 @@ repositories {
 }
 
 dependencies {
-    implementation(group="org.kohsuke", name="wordnet-random-name", version= "1.3")
-    implementation(group = "joda-time", name = "joda-time", version = "2.10.6")
-    implementation(group="com.fasterxml.jackson.datatype", name="jackson-datatype-joda", version="2.11.3")
-    implementation("net.dv8tion:JDA:4.3.0_291")
-    implementation("com.sedmelluq:lavaplayer:1.3.75")
+    implementation(group="org.kohsuke", name="wordnet-random-name", version= "1.5")
+    implementation(group = "joda-time", name = "joda-time", version = "2.10.10")
+    implementation(group="com.fasterxml.jackson.datatype", name="jackson-datatype-joda", version="2.12.4")
+    implementation("net.dv8tion:JDA:4.3.0_294")
+    implementation("com.sedmelluq:lavaplayer:1.3.77")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -68,7 +68,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc") // JVM dependency
-    implementation(group="io.ktor", name="ktor-client-cio", version="1.5.2")
+    implementation(group="io.ktor", name="ktor-client-cio", version="1.6.1")
     implementation("me.xdrop:fuzzywuzzy:1.3.1")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -77,11 +77,11 @@ dependencies {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
     testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.4.3") // for kotest framework
-    testImplementation ("io.kotest:kotest-assertions-core-jvm:4.4.3" )// for kotest core jvm assertions
-    testImplementation ("io.kotest:kotest-property-jvm:4.4.3")// for kotest property test
-    testImplementation (group="org.mockito", name="mockito-core", version="3.7.7")
-    testImplementation (group="org.mockito", name="mockito-inline", version="3.7.7")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0") // for kotest framework
+    testImplementation ("io.kotest:kotest-assertions-core-jvm:4.6.0" )// for kotest core jvm assertions
+    testImplementation ("io.kotest:kotest-property-jvm:4.6.0")// for kotest property test
+    testImplementation (group="org.mockito", name="mockito-core", version="3.11.2")
+    testImplementation (group="org.mockito", name="mockito-inline", version="3.11.2")
     testImplementation(group="org.hamcrest", name="hamcrest-all", version="1.3")
 }
 
@@ -92,6 +92,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "15"
+        jvmTarget = "16"
     }
 }
