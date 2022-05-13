c48f0f2a4dbb098b88c244e3ccc6d632b1347501
cogboy <danielgharman@outlook.com>
2022-05-13 19:46:59 +0100

    Upgrade gradle, deps and java version

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM gradle:jdk16 AS build
+FROM gradle:jdk17 AS build
 COPY . /app/
 RUN cd /app && gradle --build-cache assemble
 
-FROM openjdk:16-slim
+FROM openjdk:17-slim
 RUN mkdir /app
 COPY --from=build /app/build/libs/*.jar /app/KotlinBot.jar
 ENTRYPOINT ["java", "-XX:+UnlockExperimentalVMOptions", "-Djava.security.egd=file:/dev/./urandom","-jar","/app/KotlinBot.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,19 @@ import me.qoomon.gradle.gitversioning.GitVersioningPluginConfig
 import me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.*
 
 plugins {
-    id("org.springframework.boot") version "2.5.6"
+    id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
-    kotlin("kapt") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("kapt") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
     id("me.qoomon.git-versioning") version "4.2.1"
     id("org.barfuin.gradle.jacocolog") version "2.0.0"
     jacoco
 }
 
 group = "uk.me.danielharman"
-java.sourceCompatibility = JavaVersion.VERSION_16
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 version = "Kobot"
 gitVersioning.apply(closureOf<GitVersioningPluginConfig> {
@@ -41,9 +41,9 @@ tasks.test {
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
     reports {
-        xml.isEnabled = true
-        csv.isEnabled = false
-        html.isEnabled = true
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
     }
 }
 
@@ -56,27 +56,27 @@ repositories {
 }
 
 dependencies {
-    implementation(group = "joda-time", name = "joda-time", version = "2.10.10")
-    implementation(group="com.fasterxml.jackson.datatype", name="jackson-datatype-joda", version="2.12.4")
-    implementation("net.dv8tion:JDA:4.3.0_346")
+    implementation(group = "joda-time", name = "joda-time", version = "2.10.14")
+    implementation(group="com.fasterxml.jackson.datatype", name="jackson-datatype-joda", version="2.13.2")
+    implementation("net.dv8tion:JDA:4.4.0_350")
     implementation("com.sedmelluq:lavaplayer:1.3.78")
-    implementation("org.springframework.boot:spring-boot-starter-security:2.5.6")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.5.6")
-    implementation("org.springframework.boot:spring-boot-starter-web:2.5.6")
-    implementation("org.springframework.boot:spring-boot-starter-actuator:2.5.6")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.6.7")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.6.7")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.7")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:2.6.7")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc") // JVM dependency
     implementation(group="io.ktor", name="ktor-client-cio", version="1.6.5")
-    implementation("me.xdrop:fuzzywuzzy:1.3.1")
+    implementation("me.xdrop:fuzzywuzzy:1.4.0")
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools:2.5.6")
+    developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.7")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:2.5.6")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.0.0")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.4.5")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.3") // for kotest framework
     testImplementation ("io.kotest:kotest-assertions-core-jvm:4.6.3")// for kotest core jvm assertions
     testImplementation ("io.kotest:kotest-property-jvm:4.6.3")// for kotest property test
@@ -96,6 +96,6 @@ tasks.getByName<Jar>("jar") {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "16"
+        jvmTarget = "17"
     }
 }
