09399d378342e3e0a7b88024bf4524ee725cd23f
cogboy <danielgharman@outlook.com>
2022-06-12 22:28:26 +0100

    A maybe fix for failing tests?

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.4.5")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.4.6")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.3") // for kotest framework
     testImplementation ("io.kotest:kotest-assertions-core-jvm:4.6.3")// for kotest core jvm assertions
     testImplementation ("io.kotest:kotest-property-jvm:4.6.3")// for kotest property test
