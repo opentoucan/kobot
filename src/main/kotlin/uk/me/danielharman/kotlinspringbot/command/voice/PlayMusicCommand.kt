d0c85c2340b99332ad3e7000d6d1d463ecea0036
Daniel Harman <danielgharman@outlook.com>
2025-01-18 16:25:07 +0000

    Revert "Remove audio functionality (#5)"
    This reverts commit 00959b02f39093e8b6e1a286a671d3013fe2b10b.

--- src/main/kotlin/uk/me/danielharman/kotlinspringbot/command/voice/PlayMusicCommand.kt
+++ src/main/kotlin/uk/me/danielharman/kotlinspringbot/command/voice/PlayMusicCommand.kt
@@ -1,14 +1,102 @@
 package uk.me.danielharman.kotlinspringbot.command.voice
 
+import net.dv8tion.jda.api.entities.VoiceChannel
 import org.springframework.stereotype.Component
+import uk.me.danielharman.kotlinspringbot.KotlinBotProperties
+import uk.me.danielharman.kotlinspringbot.audio.NewAudioResultHandler
 import uk.me.danielharman.kotlinspringbot.command.interfaces.Command
 import uk.me.danielharman.kotlinspringbot.command.interfaces.ISlashCommand
 import uk.me.danielharman.kotlinspringbot.events.DiscordMessageEvent
+import uk.me.danielharman.kotlinspringbot.helpers.Embeds
+import uk.me.danielharman.kotlinspringbot.helpers.Failure
+import uk.me.danielharman.kotlinspringbot.helpers.Success
+import uk.me.danielharman.kotlinspringbot.models.CommandParameter
+import uk.me.danielharman.kotlinspringbot.models.CommandParameter.ParamType
+import uk.me.danielharman.kotlinspringbot.provider.GuildMusicPlayerProvider
+import uk.me.danielharman.kotlinspringbot.services.DiscordActionService
+import uk.me.danielharman.kotlinspringbot.services.DiscordCommandService
+import uk.me.danielharman.kotlinspringbot.services.SpringGuildService
 
 @Component
-class PlayMusicCommand : Command(
-    "play", "Play audio via Youtube, Vimeo etc."), ISlashCommand {
+class PlayMusicCommand(
+    private val guildMusicPlayerProvider: GuildMusicPlayerProvider,
+    private val springGuildService: SpringGuildService,
+    private val discordActionService: DiscordActionService,
+    private val discordCommandService: DiscordCommandService,
+    private val kotlinBotProperties: KotlinBotProperties
+) : Command(
+    "play", "Play audio via Youtube, Vimeo etc.",
+    listOf(CommandParameter(0, "url", ParamType.Word, "Url to play music from"))
+), ISlashCommand {
+
     override fun execute(event: DiscordMessageEvent) {
-        event.reply("Playing music has been disabled. Please use alternative bot.")
+        val voiceChannel: VoiceChannel?
+
+        val guild = event.guild
+
+        if (guild == null) {
+            event.reply(Embeds.createErrorEmbed("This command can only be used in Servers"))
+            return
+        }
+
+        val paramValue = event.getParamValue(this.commandParameters[0])
+        var url = paramValue.asString()
+
+        if (url == null || paramValue.error) {
+            val player = guildMusicPlayerProvider.getGuildAudioPlayer(guild).player
+            player.isPaused = !player.isPaused
+
+            val message = if (player.isPaused) "Paused" else "Playing"
+            event.reply(message)
+            return
+        }
+
+        if (url.startsWith(kotlinBotProperties.commandPrefix)){
+           url = when(val command = discordCommandService.getCommand(guild.id, url.drop(1))){
+               is Failure -> url
+               is Success -> command.value.content?.trim()
+           }
+        }
+
+        val member = guild.retrieveMember(event.author).complete()
+
+        if (member == null) {
+            event.reply("Can't find member!!!!")
+            return
+        }
+
+        val voiceState = member.voiceState
+
+        if (voiceState == null) {
+            event.reply("Can't find member voicestate! Are you in a channel?")
+            return
+        }
+
+        voiceChannel = voiceState.channel
+
+        if (voiceChannel == null) {
+            event.reply("Can't find voice channel! Are you in a channel?")
+            return
+        }
+        when (val botVoiceChannel = discordActionService.getBotVoiceChannel(guild.id)) {
+            is Success -> {
+                logger.info("Bot's voice channel: " + botVoiceChannel.value.id)
+                guild.audioManager.openAudioConnection(botVoiceChannel.value)
+            }
+        }
+
+        if (voiceChannel != guild.audioManager.connectedChannel) {
+            logger.info("My voice channel: " + voiceChannel.id)
+            guild.audioManager.closeAudioConnection()
+            guild.audioManager.openAudioConnection(voiceChannel)
+        }
+
+        logger.info("Connected voice channel from manager: " + guild.audioManager.connectedChannel?.id)
+        val musicManager = guildMusicPlayerProvider.getGuildAudioPlayer(voiceChannel.guild)
+        guildMusicPlayerProvider.playerManager.loadItemOrdered(
+            musicManager,
+            url,
+            NewAudioResultHandler(voiceChannel, musicManager, event, springGuildService, guild)
+        )
     }
 }
---
+++ src/main/kotlin/uk/me/danielharman/kotlinspringbot/command/voice/SummonCommand.kt
@@ -0,0 +1,49 @@
+package uk.me.danielharman.kotlinspringbot.command.voice
+
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException
+import org.springframework.stereotype.Component
+import uk.me.danielharman.kotlinspringbot.command.interfaces.Command
+import uk.me.danielharman.kotlinspringbot.command.interfaces.ISlashCommand
+import uk.me.danielharman.kotlinspringbot.events.DiscordMessageEvent
+import uk.me.danielharman.kotlinspringbot.helpers.Embeds
+
+@Component
+class SummonCommand : Command("summon", "Make the bot join the voice channel"), ISlashCommand {
+
+    override fun execute(event: DiscordMessageEvent) {
+        if (event.guild == null) {
+            event.reply(Embeds.createErrorEmbed("This command can only be used in Servers"))
+            return
+        }
+
+        val audioManager = event.guild.audioManager
+        val member = event.guild.retrieveMember(event.author).complete()
+
+        if (member == null) {
+            event.reply("Could not find member.")
+            return
+        }
+
+        val voiceState = member.voiceState
+
+        if (voiceState == null) {
+            event.reply("You don't seem to be in a channel.")
+            return
+        }
+
+        val voiceChannel = voiceState.channel
+
+        if (voiceChannel == null) {
+            event.reply("You don't seem to be in a channel.")
+            return
+        }
+
+        try {
+            audioManager.openAudioConnection(voiceChannel)
+        } catch (e: InsufficientPermissionException) {
+            logger.error("Bot encountered an exception when attempting to join a voice channel ${e.message}")
+            event.reply("I don't have permission to join.")
+        }
+    }
+
+}
