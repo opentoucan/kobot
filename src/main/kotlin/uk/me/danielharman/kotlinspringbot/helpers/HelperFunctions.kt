ad7f2d29ba4f0681029e8d45bc6664e2f8842c81
Daniel Harman <DanielHarman@users.noreply.github.com>
2025-02-06 21:50:10 +0000

    Add sending of now playing messages and display queued tracks command (#55)

--- src/main/kotlin/uk/me/danielharman/kotlinspringbot/helpers/HelperFunctions.kt
+++ src/main/kotlin/uk/me/danielharman/kotlinspringbot/helpers/HelperFunctions.kt
@@ -1,7 +1,15 @@
 package uk.me.danielharman.kotlinspringbot.helpers
 
+import org.apache.commons.lang3.time.DurationFormatUtils
+
 object HelperFunctions {
     fun <A, B, C> partialWrapper(f: (A, B) -> C, a: A): (B) -> C {
         return { b: B -> f(a, b)}
     }
+
+    private const val TIME_FORMAT = "mm:ss"
+    fun Long.formatDurationString(): String {
+        return DurationFormatUtils.formatDuration(this, TIME_FORMAT)
+    }
+
 }
